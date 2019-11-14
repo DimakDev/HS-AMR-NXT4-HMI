7f664bd66f27fe6ac44ab492f28f8017ea6c2159
MonKosmo <57316658+MonKosmo@users.noreply.github.com>
2019-11-14 13:40:04 +0100

    .

--- HS AMR/ViewController.swift
+++ HS AMR/ViewController.swift
@@ -7,14 +7,137 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+let svcBT05 = CBUUID.init(string: "FFE0")
+let charBT05 = CBUUID.init(string: "FFE1")
 
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            if let pUUIDString = UserDefaults.standard.object(forKey: "PUUID") as? String {
+                if let pUUID = UUID.init(uuidString: pUUIDString) {
+                    let peripherals = centralManager.retrievePeripherals(withIdentifiers: [pUUID])
+                    if let p = peripherals.first {
+                        connect(toPeripheral: p)
+                        return
+                    }
+                }
+            }
+            let peripherals = centralManager.retrieveConnectedPeripherals(withServices: [CBUUID.init(string: "FFE0")])
+            if let p = peripherals.first {
+                connect(toPeripheral: p)
+                return
+            }
+            central.scanForPeripherals(withServices: [CBUUID.init(string: "FFE0")], options: nil)
+            print ("scanning...")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name?.contains("BT05") == true {
+            print (advertisementData)
+            connect(toPeripheral: peripheral)
+        }
+    }
+    
+    func connect(toPeripheral: CBPeripheral) {
+        print (toPeripheral.name ?? "no name")
+        centralManager.stopScan()
+        centralManager.connect(toPeripheral, options: nil)
+        myPeripheral = toPeripheral
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        central.scanForPeripherals(withServices: [CBUUID.init(string: "FFE0")], options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        central.scanForPeripherals(withServices: [CBUUID.init(string: "FFE0")], options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print ("connected \(peripheral.name!)")
+        peripheral.discoverServices(nil)
+        peripheral.delegate = self
+        UserDefaults.standard.set(peripheral.identifier.uuidString, forKey: "PUUID")
+        UserDefaults.standard.synchronize()
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for svc in services {
+                if svc.uuid == svcBT05 {
+                    print (svc.uuid.uuidString)
+                    peripheral.discoverCharacteristics(nil, for: svc)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let chars = service.characteristics {
+            for char in chars {
+                print (char.uuid.uuidString)
+                if char.uuid == charBT05 {
+                    myCharasteristic = char
+                    peripheral.setNotifyValue(true, for: char)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let val = characteristic.value {
+            if characteristic.uuid == charBT05 {
+                print ("keys: \([UInt8](val))")
+            }
+        }
+    }
+    
+    func writeValue(command: String) {
+        let dataToSend: Data = command.data(using: String.Encoding.utf8)!
+        if myCharasteristic!.properties.contains(CBCharacteristicProperties.writeWithoutResponse) {
+            myPeripheral!.writeValue(dataToSend, for: myCharasteristic!, type: CBCharacteristicWriteType.withoutResponse)
+        }
+        else {
+            myPeripheral!.writeValue(dataToSend, for: myCharasteristic!, type: CBCharacteristicWriteType.withResponse)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        print ("wrote value")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager.init(delegate: self, queue: nil)
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var centralManager : CBCentralManager!
+    var myPeripheral : CBPeripheral?
+    var myCharasteristic : CBCharacteristic?
+    
+    
+    
+    //Interactive section for communication between UI and Arduino UNO
+    @IBAction func Power(_ sender: Any) {
+        writeValue(command: "on")
+    }
+    
+    @IBAction func Park(_ sender: Any) {
+        writeValue(command: "park")
+    }
+    @IBAction func Run(_ sender: Any) {
+        writeValue(command: "scout")
+    }
+    
+    
+    
 }
-
