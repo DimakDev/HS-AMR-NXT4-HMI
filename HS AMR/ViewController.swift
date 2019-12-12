ae2eb65d23feb633381a0c4cb62b05784ba93d3c
MonKosmo <57316658+MonKosmo@users.noreply.github.com>
2019-12-12 12:52:28 +0100

    Map added

--- HS AMR/ViewController.swift
+++ HS AMR/ViewController.swift
@@ -13,6 +13,7 @@ let svcBT05 = CBUUID.init(string: "FFE0")
 let charBT05 = CBUUID.init(string: "FFE1")
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+   
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == CBManagerState.poweredOn {
             if let pUUIDString = UserDefaults.standard.object(forKey: "PUUID") as? String {
@@ -63,6 +64,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         print ("disconnected \(peripheral.name!)")
         Disconnect.setOn(false, animated: true)
+        writeValue(data: writeModePause)
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
@@ -100,7 +102,9 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         if let val = characteristic.value {
             if characteristic.uuid == charBT05 {
-                print ("modeID: \([UInt8](val))")
+                DetailsController.input = [UInt8](val)
+                detailsController.prozessArray(array: DetailsController.input)
+                print ("Array size: \(DetailsController.input.count) Array: \(DetailsController.input)")
             }
         }
     }
@@ -117,11 +121,13 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
         print ("wrote value")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         centralManager = CBCentralManager.init(delegate: self, queue: nil)
+        
+        createMap()
     }
     
     override func didReceiveMemoryWarning() {
@@ -134,17 +140,24 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     var myPeripheral : CBPeripheral?
     var myCharasteristic : CBCharacteristic?
     
+    var detailsController = DetailsController()    
     
-    //Interactive section for communication between UI and Arduino UNO
+    //UI interaction section
     @IBOutlet weak var Disconnect: UISwitch!
     @IBOutlet weak var Parking: UIButton!
     @IBOutlet weak var Run: UIButton!
     
     var toggled = false
     
-    let modeParking:[UInt8] = [0x00, 0x01]
-    let modeScout:[UInt8] = [0x00, 0x00]
-    let modePause:[UInt8] = [0x00, 0x03]
+    var openInputStream: [UInt8] = [0x09, 0x00]
+    var closeInputStream: [UInt8] = [0x08, 0x00]
+    
+    var modeParking:[UInt8] = [0x00, 0x01]
+    var modeScout:[UInt8] = [0x00, 0x00]
+    var modePause:[UInt8] = [0x00, 0x03]
+
+    lazy var writeOpenStream =  Data(bytes: openInputStream)
+    lazy var writeCloseStream =  Data(bytes: closeInputStream)
 
     lazy var writeModeParking =  Data(bytes: modeParking)
     lazy var writeModeScout =  Data(bytes: modeScout)
@@ -169,12 +182,11 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         }
     }
     
-    // modeID [02] for Parking_THIS
     @IBAction func Parking(_ sender: UIButton) {
-        UIButton.animate(withDuration: 0.1, animations: {sender.transform = CGAffineTransform(scaleX: 1.1, y: 1.15)}, completion: {finish in UIButton.animate(withDuration: 0.1, animations: {sender.transform = CGAffineTransform.identity})})
         if myCharasteristic != nil {
         writeValue(data: writeModeParking)
         }
+        UIButton.animate(withDuration: 0.1, animations: {sender.transform = CGAffineTransform(scaleX: 1.1, y: 1.15)}, completion: {finish in UIButton.animate(withDuration: 0.1, animations: {sender.transform = CGAffineTransform.identity})})
     }
     
     @IBAction func Run(_ sender: UIButton) {
@@ -192,6 +204,82 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         UIButton.animate(withDuration: 0.1, animations: {sender.transform = CGAffineTransform(scaleX: 1.1, y: 1.15)}, completion: {finish in UIButton.animate(withDuration: 0.1, animations: {sender.transform = CGAffineTransform.identity})})
     }
     
+    
+    @IBAction func detailsButton(_ sender: Any) {
+        performSegue(withIdentifier: "detailsSegue", sender: self)
+        if myCharasteristic != nil {
+            writeValue(data: writeOpenStream)
+        }
+    }
+    
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! DetailsController
+        vc.printBuffer = GlobalVariable.inputBuffer
+    }
+    */
+ 
     @IBAction func unwindToGlobal(segue: UIStoryboardSegue) {
+        if myCharasteristic != nil {
+            writeValue(data: writeCloseStream)
+        }
+    }
+    
+    func createMap() {
+        //let screenSize: CGRect = UIScreen.main.bounds
+        
+        // get screen width.
+        // let screenWidth = screenSize.width
+        
+        // get screen height.
+        // let screenHeight = screenSize.height
+        
+        // Create a CGRect object which is used to render a rectangle.
+        let leftArea: CGRect = CGRect(x: 15, y: 135, width: 77, height: 344)
+        let rightArea: CGRect = CGRect(x: 228, y: 218, width: 82, height: 181)
+        let buttomArea: CGRect = CGRect(x: 125, y: 549, width: 138, height: 73)
+        
+        // Create a UIView object which use above CGRect object.
+        let leftParking = UIView(frame: leftArea)
+        let rightParking = UIView(frame: rightArea)
+        let buttomParking = UIView(frame: buttomArea)
+        
+        // Set UIView background color.
+        leftParking.backgroundColor = UIColor.lightGray
+        rightParking.backgroundColor = UIColor.lightGray
+        buttomParking.backgroundColor = UIColor.lightGray
+        
+        // Add above UIView object as the main view's subview.
+        self.view.addSubview(leftParking)
+        self.view.addSubview(rightParking)
+        self.view.addSubview(buttomParking)
+        
+        //design the path
+        let road = UIBezierPath()
+        road.move(to: CGPoint(x: 124, y: 494))
+        road.addLine(to: CGPoint(x: 263, y: 494))
+        road.addLine(to: CGPoint(x: 263, y: 426))
+        road.addLine(to: CGPoint(x: 194, y: 426))
+        road.addLine(to: CGPoint(x: 194, y: 162))
+        road.addLine(to: CGPoint(x: 263, y: 162))
+        road.addLine(to: CGPoint(x: 263, y: 95))
+        road.addLine(to: CGPoint(x: 124, y: 95))
+        road.close()
+        
+        //design path in layer
+        let roadLayer = CAShapeLayer()
+        roadLayer.path = road.cgPath
+        roadLayer.fillColor = UIColor.clear.cgColor
+        roadLayer.strokeColor = UIColor.lightGray.cgColor
+        roadLayer.lineWidth = 40.0
+        
+        let pathLayer = CAShapeLayer()
+        pathLayer.path = road.cgPath
+        pathLayer.fillColor = UIColor.clear.cgColor
+        pathLayer.strokeColor = UIColor.darkGray.cgColor
+        pathLayer.lineWidth = 8.0
+        
+        view.layer.addSublayer(roadLayer)
+        view.layer.addSublayer(pathLayer)
     }
 }
